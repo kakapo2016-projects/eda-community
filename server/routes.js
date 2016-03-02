54f854be61b18a8da7f8df4a86c5e8a982122059
Nicholas r Maskell <nick@Nicholass-MacBook-Pro.local>
2016-03-03 12:12:03 +1300

    base of oauth setup

--- server/routes.js
+++ server/routes.js
@@ -4,4 +4,15 @@ exports = module.exports = function (app) {
   app.get('/', function (req, res) {
   })
 
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/community');
+  });
+
+
 }
--- server/server.js
+++ server/server.js
@@ -9,8 +9,24 @@ var session = require('express-session')
 var app = express() // create the express application
 var server = require('http').createServer(app) // create the server
 var routes = require('./routes')
+var GitHubStrategy = require('passport-github').Strategy;
 
 routes(app)
+
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.Client_ID,
+    clientSecret: process.env.Client_Secret,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
 
 
 if (require.main === module) {
