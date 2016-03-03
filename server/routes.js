159d7a1588553f23aafb2005ac1c5f9e9a957efa
Richard Joe <rjoe19@hotmail.com>
2016-03-03 16:20:38 +1300

    add community

--- server/routes.js
+++ server/routes.js
@@ -17,10 +17,18 @@ exports = module.exports = function (app) {
       res.render('signup');
   });
 
+  app.get('/community', function (req, res) {
+    var user = [{'id': 12, displayName: 'rjoe19', profileUrl: 'www.google.com', email: 'rjoe@gmail.com', photoUrl: 'www.fillmurray.com/200/200'},
+    {'id': 12, displayName: 'rjoe19', profileUrl: 'www.google.com', email: 'rjoe@gmail.com', photoUrl: 'www.fillmurray.com/200/200'}]
+
+      res.render('community', {user: user});
+
+  });
+
 app.get('/auth/github',
   passport.authenticate('github'));
 
-app.get('/auth/github/callback', 
+app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
