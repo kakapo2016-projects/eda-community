d5155806d052b47ec4fd53609a92e767e6b05217
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-03 16:28:27 +1300

    adding post and get for /info/:id

--- server/routes.js
+++ server/routes.js
@@ -27,44 +27,42 @@ app.get('/auth/github/callback',
     res.redirect('/community');
   });
 
-  // app.post('/info/:UserID', function(req, res){
-  //   // save the extra info about this user back to the database
-  //    knex('users').insert({
-  //      id: UserID,
-  //      about: req.body.about,
-  //    }) .then (function(resp){
-  //      var rows = [{id: UserID, skillDescr: req.body.skill[0]},
-  //       {id: UserID, description: req.body.skill[1]},
-  //       {id: UserID, description: req.body.skill[2]},
-  //       {id: UserID, description: req.body.skill[3]},
-  //       {id: UserID, description: req.body.skill[4]}];
-  //       var chunkSize = 5;
-  //       knex.batchInsert('skills', rows, chunkSize)
-  //       .then(function(respo) {
-  //         res.redirect('/community')
-  //       })
-  //
-  //      })
-  //    })
-  //
-  // })
+  app.post('/info/:UserID', function(req, res){
+    // save the extra info about this user back to the database
+     knex('users').insert({
+       id: UserID,
+       about: req.body.about,
+     }) .then (function(resp){
+       var rows = [{id: UserID, skillDescr: req.body.skill[0]},
+        {id: UserID, description: req.body.skill[1]},
+        {id: UserID, description: req.body.skill[2]},
+        {id: UserID, description: req.body.skill[3]},
+        {id: UserID, description: req.body.skill[4]}];
+        var chunkSize = 5;
+        knex.batchInsert('skills', rows, chunkSize)
+        .then(function(respo) {
+          res.redirect('/community')
+        })
+       })
+     })
 
+  app.get('/info/:UserID', function(req, res){
+    // get the extra info about this user from the database
+     knex('users').where(
+     'id', UserID
+     )
+     .select('about', 'id')
+     .then(function(resp) {
+      if (resp.length > 0) {
+       knex('skills').where('userid', UserID)
+       .select('id',  'description')
+       .then( function(respo){
+         res.redirect('/community')
+      })
+     }
 
-  // app.get('/info/:UserID', function(req, res){
-  //   // get the extra info about this user from the database
-  //   knex('users').where(
-  //   'id', UserID
-  //   ).select('about', 'id').then(function(resp) {
-  //   if (resp.length > 0) {
-  //     knex('skills').where('userid', UserID)
-  //     .select('id',  'description').then( function(respo){
-  //       res.redirect('/community')
-  //     })
-  //
-  //     })
-  //   }
-  //
-  //
-  // })
+    })
+
+  })
 
 }
